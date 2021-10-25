4540ba0fd98a1759fdeaf01355805e039cf5280d
Phimos <41637559+Phimos@users.noreply.github.com>
2021-10-25 14:11:05 +0800

    <feat>: add kernel fisher

--- src/nonlinear.py
+++ src/nonlinear.py
@@ -72,6 +72,7 @@ class MultilayerPerceptrons(ClassifierEstimator):
 class nonlinearSVM(ClassifierEstimator):
     def __init__(self, n_samples: int = 10000, in_features: int = 56, C: float = 1.0, kernel: str = "rbf", **kwargs: Any) -> None:
         super().__init__()
+        self.eps = 1e-8
         self.C = C
         self.kernel = kernel
         self.kernel_func = self.select_kernel_func(kernel, **kwargs)
@@ -100,41 +101,72 @@ class nonlinearSVM(ClassifierEstimator):
 
     def fit(self, X: Tensor, y: Tensor) -> None:
         self.X = X
-        self.y = torch.sign(y.float() - 0.5)
+        self.y = torch.sign(y.float() - 0.5).reshape(-1, 1)
         self.alpha = torch.zeros(X.shape[0], 1)
+        self.K = self.kernel_func(X, X)
 
-        xi = F.relu(
-            1 - self.y * ((self.y.T * self.alpha.T * self.kernel_func(X, X)).sum(dim=-1) + self.b))
-        pass
+        for i in range(100):
+            self.fx = self.forward(self.X).reshape(-1, 1)
+            self.E = self.fx - self.y
+            idx1, idx2 = self.select_indices()
+            self.alpha = self.update_alpha(
+                idx1, idx2, self.alpha, self.y, self.K)
+            self.b = self.update_b()
+            if i % 10 == 0:
+                print(self.objective_function(),
+                      self.dual_objective_function())
+                print(self.score(X, y))
 
-    def objective_function(self, xi: Tensor):
-        return 0.5 * self.w.norm() ** 2 + self.C * xi.sum()
+    def objective_function(self):
+        xi = F.relu(1 - self.y * self.fx)
+        print(xi.shape)
+        return 0.5 * ((self.y @ self.y.T) * (self.alpha @ self.alpha.T) * self.K).sum() + self.C * xi.sum()
 
-    def dual_objective_function(self, X: Tensor, y: Tensor, alpha: Tensor, kernel_func: Callable):
-        return alpha.sum() - 0.5 * ((y @ y.T) * (alpha @ alpha.T) * kernel_func(X, X)).sum()
+    def dual_objective_function(self):
+        return self.alpha.sum() - 0.5 * ((self.y @ self.y.T) * (self.alpha @ self.alpha.T) * self.K).sum()
 
-    def update_alpha(self, idx1, idx2, alpha, y, E, K):
+    def update_alpha(self, idx1, idx2, alpha, y, K):
         if y[idx1] != y[idx2]:
             U = torch.clamp(alpha[idx2] - alpha[idx1], min=0)
             V = torch.clamp(self.C - alpha[idx1] + alpha[idx2], max=self.C)
         else:
             U = torch.clamp(alpha[idx1] + alpha[idx2] - self.C, min=0)
             V = torch.clamp(alpha[idx1] + alpha[idx2], max=self.C)
+        U, V = U.item(), V.item()
+        print("U", U, "V", V)
+        E = self.E
 
-        E = (alpha.T * y.T * K).sum(dim=-1) + self.b - y
-
-        alpha_2_old = alpha[idx2]
+        alpha_2_old = alpha[idx2].item()
         alpha[idx2] += y[idx2] * (E[idx1] - E[idx2]) / \
             (K[idx1, idx1] + K[idx2, idx2] - 2 * K[idx1, idx2])
         alpha[idx2] = torch.clamp(alpha[idx2], min=U, max=V)
         alpha[idx1] += y[idx1] * y[idx2] * (alpha_2_old - alpha[idx2])
         return alpha
 
-    def select_index1(self):
-        pass
+    def update_b(self):
+        cond = ((self.alpha > 0) & (self.alpha < self.C))
+        if cond.any():
+            return (self.y - self.fx)[cond].mean().item()
+        else:
+            return 0
 
-    def select_index2(self):
-        pass
+    def select_index1(self):
+        out = self.y.squeeze() * self.forward(self.X) - 1
+        alpha = self.alpha.squeeze()
+        cond1 = (out > 0) & (alpha == 0)
+        cond2 = (out == 0) & (alpha > 0) & (alpha < self.C)
+        cond3 = (out < 0) & (alpha == self.C)
+        kkt = cond1 | cond2 | cond3
+        return torch.nonzero(~kkt)[0].item()
+
+    def select_index2(self, idx1):
+        E = self.E
+        return torch.argmax(torch.abs(E[idx1] - E)).item()
+
+    def select_indices(self):
+        idx1 = self.select_index1()
+        idx2 = self.select_index2(idx1)
+        return idx1, idx2
 
     def forward(self, X: Tensor) -> Tensor:
         return (self.alpha.T * self.y.T * self.kernel_func(X, self.X)).sum(dim=-1) + self.b
@@ -143,7 +175,61 @@ class nonlinearSVM(ClassifierEstimator):
         return torch.relu(torch.sign(self.forward(X)))
 
 
+class KernelFisher(ClassifierEstimator):
+    def __init__(self, kernel: str = "rbf", t: float = 0.01, ** kwargs: Any) -> None:
+        super().__init__()
+        self.eps = 1e-8
+        self.t = t
+        self.kernel = kernel
+        self.kernel_func = self.select_kernel_func(kernel, **kwargs)
+
+        self.alpha = None
+        self.b = None
+
+    def select_kernel_func(self, kernel: str, **kwargs: Any):
+        def linear(x: Tensor, y: Tensor):
+            return torch.dot(x, y.T)
+
+        def rbf(x: Tensor, y: Tensor, gamma: float):
+            return torch.exp(-gamma * torch.cdist(x, y, p=2))
+
+        def polynomial(x: Tensor, y: Tensor, degree: float):
+            return (linear(x, y) + 1) ** degree
+
+        if kernel == "linear":
+            return linear
+        elif kernel == "rbf":
+            return partial(rbf, gamma=kwargs.get("gamma", 1.0))
+        elif kernel == "poly":
+            return partial(polynomial, degree=kwargs.get("degree", 3.0))
+        else:
+            raise ValueError("Unknown kernel function")
+
+    def fit(self, X: Tensor, y: Tensor) -> None:
+        self.X = X
+        K = self.kernel_func(X, X)
+        mask0, mask1 = y == 0, y == 1
+        K0, K1 = K[mask0], K[mask1]
+        m0 = K0.mean(dim=0)
+        m1 = K1.mean(dim=0)
+        tau = (m1 - m0).reshape(-1, 1)
+        d0 = K0 - m0
+        d1 = K1 - m1
+        N0 = d0.T @ d0
+        N1 = d1.T @ d1
+        N = N0 + N1
+        self.alpha = torch.pinverse(N + self.t * K) @ tau
+        self.b = -(0.5 * (m0 + m1)) @ self.alpha
+
+    def predict(self, X: Tensor) -> Tensor:
+        assert self.alpha is not None and self.b is not None
+        ret = (self.alpha * self.kernel_func(self.X, X)).sum(dim=0) + self.b
+        return torch.relu(torch.sign(ret))
+
+
 if __name__ == '__main__':
+    torch.random.manual_seed(42)
+
     # Load data
     data = loadmat('data.mat')['data']
     X, y = data[:, :-1], data[:, -1]
@@ -157,6 +243,16 @@ if __name__ == '__main__':
     X_train, X_test = X[indices[:train_size]], X[indices[train_size:]]
     y_train, y_test = y[indices[:train_size]], y[indices[train_size:]]
 
-    model = MultilayerPerceptrons()
+    #model = MultilayerPerceptrons()
+    #model.fit(X_train, y_train)
+    #print("Multilayer Perceptrons Accuracy:", model.score(X_test, y_test))
+
+    model = KernelFisher(kernel="rbf", gamma=1.0)
     model.fit(X_train, y_train)
-    print("Multilayer Perceptrons Accuracy:", model.score(X_test, y_test))
+    print(model.score(X_train, y_train))
+    print(model.score(X_test, y_test))
+
+    model = KernelFisher(kernel="poly", degree=3.0)
+    model.fit(X_train, y_train)
+    print(model.score(X_train, y_train))
+    print(model.score(X_test, y_test))
